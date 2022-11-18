948de687d96b7ce3a678a0ed79197ec0e7a44ac1
milletgilmore <milletgilmore@gmail.com>
2022-11-18 07:58:58 -0600

    changed file types back to images

--- middleware/multer.js
+++ middleware/multer.js
@@ -5,7 +5,7 @@ module.exports = multer({
   storage: multer.diskStorage({}),
   fileFilter: (req, file, cb) => {
     let ext = path.extname(file.originalname);
-    if (ext !== ".jpg" || ext !== ".jpeg" || ext !== ".png" || ext !== ".mp4" || ext !== ".mp3" || ext !== ".mov") {
+    if (ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png") {
       cb(new Error("File type is not supported"), false);
       return;
     }
