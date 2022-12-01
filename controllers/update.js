7fc7a89b92c31df181f38d41fde2d6faf665e584
milletgilmore <milletgilmore@gmail.com>
2022-11-30 20:57:19 -0600

    added the dicover controller

--- controllers/update.js
+++ controllers/update.js
@@ -31,6 +31,22 @@ module.exports = {
       console.log(err);
     }
   },
+  getDiscover: async (req, res) => { 
+    console.log(req.user)
+    try {
+      //Since we have a session each request (req) contains the logged-in users info: req.user
+      //console.log(req.user) to see everything
+      //Grabbing just the posts of the logged-in user
+      const update = await Update.find({ user: req.user.id }).populate('update');
+
+     console.log(update)
+
+     //Sending post data from mongodb and user data to ejs template
+     res.render("discover.ejs", { update: update , user: req.user });
+   } catch (err) {
+     console.log(err);
+   }
+ },
   getUpdate: async (req, res) => {
     try {
       //id parameter comes from the post routes
